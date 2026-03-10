e0239f6d794a45db3b8def33dffe385fd60337e7
Petro <pkrynytskyi3@gmail.com>
2026-03-10 17:29:25 +0200

    change delete button

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  todoParent.addEventListener("click", (e) => {
+    const event = e.target;
+    if (event.closest(".delete-btn")) {
+      const i = +event.closest(".delete-btn").dataset.delete;
+      deleteTodo(i, todosDB, todoRender);
+    }
+  });
+
+  function deleteTodo(i, arr, cback) {
+    arr.splice(i, 1);
+    cback();
+  }
+
   function todoRender() {
     todoParent.innerHTML = "";
     todosDB.forEach((todo, index) => {
@@ -31,19 +44,13 @@ document.addEventListener("DOMContentLoaded", () => {
         
             <h2>${todo.text}</h2>
             <p>${days}days left</p>
-            <button class="delete-btn" title="Delete">
+            <button class="delete-btn" title="Delete" data-delete="${index}">
               <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="15" height="15" viewBox="0 0 24 24">
                 <path d="M 10.806641 2 C 10.289641 2 9.7956875 2.2043125 9.4296875 2.5703125 L 9 3 L 4 3 A 1.0001 1.0001 0 1 0 4 5 L 20 5 A 1.0001 1.0001 0 1 0 20 3 L 15 3 L 14.570312 2.5703125 C 14.205312 2.2043125 13.710359 2 13.193359 2 L 10.806641 2 z M 4.3652344 7 L 5.8925781 20.263672 C 6.0245781 21.253672 6.877 22 7.875 22 L 16.123047 22 C 17.121047 22 17.974422 21.254859 18.107422 20.255859 L 19.634766 7 L 4.3652344 7 z"></path>
               </svg>
             </button>
         `;
       todoParent.appendChild(todoDiv);
-
-      const deleteBtn = todoDiv.querySelector(".delete-btn");
-      deleteBtn.addEventListener("click", () => {
-        todosDB.splice(index, 1);
-        todoRender();
-      });
     });
   }
 });
